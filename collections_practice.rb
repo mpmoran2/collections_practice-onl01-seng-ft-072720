75fe5ad58c42de0c5ba7f0f90ffc91c0763e13ff
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-01 22:15:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,9 +20,13 @@ def swap_elements(array)
 end
 
 def reverse_array(array)
+  array.reverse
 end
 
 def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$"
+  end
 end
 
 def find_a(array)
